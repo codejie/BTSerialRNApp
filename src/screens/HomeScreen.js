d40c17b4fe76a2691aea4b4212915ec572982d4a
Jie <jie@dream-town.cn>
2017-05-04 12:38:05 +0800

    scanning bluetooth devices

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet, Text, View, Button, ListView } from 'react-native';
 
+import BluetoothSerial from 'react-native-bluetooth-serial';
+// import co from 'co';
+
 import styles from './styles';
 
 const ACTION_SCAN_DEVICES = 'ACTION_SCAN_DEVICES';
+const ACTION_UPDATE_DEVICES = 'ACTION_UPDATE_DEVICES';
+
 
 export function homeReducer(state = {
-    isScanning: false
+    isScanning: false,
+    devices: []
 }, action) {
+    console.log('homeReducer = ', state, action);
     switch(action.type) {
         case ACTION_SCAN_DEVICES:
-            return {
+            return Object.assign({}, state,{
                 isScanning: action.isScanning
-            };
+            });
+        case ACTION_UPDATE_DEVICES:
+            return Object.assign({}, state, {
+                devices: action.devices
+            });
         default:
             return state;
     }
@@ -35,10 +46,40 @@ class HomeScreen extends React.Component {
         this.getTitle = this.getTitle.bind(this);
         this.onButtonPress = this.onButtonPress.bind(this);
         this.getDataSource = this.getDataSource.bind(this);
+        this.initBluetoothSerial = this.initBluetoothSerial.bind(this);
+    }
+
+    initBluetoothSerial() {
+        BluetoothSerial.isEnabled()
+            .then(result => {
+                console.log('Bluetooth checked - ', result);
+                if (result) {
+                    BluetoothSerial.list()
+                        .then(values => {
+                            console.log('list result = ', values);
+                            this.props.update(values);
+                        }).catch(err => {
+                            console.log('list failed - ', err);
+                        });
+                } else {
+                    //
+                }
+            }).catch(err => {
+                console.log('Bluetooth is disabled.');
+            });
+    }
+
+    stopBluetoothSerial() {
+
     }
 
     onButtonPress() {
         console.log('press = ', this.props);
+        if (this.props.isScanning) {
+            this.stopBluetoothSerial();
+        } else {
+            this.initBluetoothSerial();
+        }
         this.props.press(!this.props.isScanning);
     }
 
@@ -47,15 +88,23 @@ class HomeScreen extends React.Component {
     }
 
     getDataSource() {
-        return this.ds.cloneWithRows(['1', '2']);
+        // return this.ds.cloneWithRows(this.props.devices);
+        console.log('getData - ', this.props.devices);
+        let names = [];
+        if (this.props.devices) {
+            for (let device of this.props.devices) {
+                names.push(device.name);
+            }
+        }
+        return this.ds.cloneWithRows(names);
     }
 
     render() {
         return (
             <View style={styles.main}>
                 <Button title={this.getTitle()} onPress={()=> this.onButtonPress()} />
-                <Text style={styles.font_24}>Hello</Text>
-                <ListView dataSource={this.getDataSource()} renderRow={(row) => <Text>{row}</Text>} />
+                {/*<Text style={styles.font_24}>Hello</Text>*/}
+                <ListView enableEmptySections={true} dataSource={this.getDataSource()} renderRow={(row) => <Text style={styles.font_24}>{row}</Text>} />
             </View>
         );
     }
@@ -64,16 +113,23 @@ class HomeScreen extends React.Component {
 HomeScreen.propTypes = {
   isScanning: React.PropTypes.bool.isRequired,
   press: React.PropTypes.func.isRequired,
+  update: React.PropTypes.func.isRequired,
+  devices: React.PropTypes.array.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-    isScanning: state.homeReducer.isScanning
+    isScanning: state.homeReducer.isScanning,
+    devices: state.homeReducer.devices
 });
 
 const mapDispatchToProps = (dispatch) => ({
     press: (isScanning) => dispatch({
         type: ACTION_SCAN_DEVICES,
         isScanning: isScanning
+    }),
+    update: (devices) => dispatch({
+        type: ACTION_UPDATE_DEVICES,
+        devices: devices
     })
 });
 
